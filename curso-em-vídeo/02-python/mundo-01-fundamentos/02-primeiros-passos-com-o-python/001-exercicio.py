a64a9c7c664bacc120af356139ba0d0e5d8a4e31
Cidy Sampaio <cidy.sampaio@gmail.com>
2024-05-25 23:41:20 -0300

    commit inicial

---
+++ curso-em-vídeo/02-python/mundo-01-fundamentos/02-primeiros-passos-com-o-python/001-exercicio.py
@@ -0,0 +1,9 @@
+"""Aula 4 - Primeiros comandos em Python3 (Python 3 | Mundo 1)
+
+001) Crie um programa que escreva "Olá, Mundo!" na tela.
+"""
+
+print('Olá, Mundo!')
+
+msg = 'Hello, World!'
+print(msg)
---
+++ curso-em-vídeo/02-python/mundo-01-fundamentos/02-primeiros-passos-com-o-python/002-exercicio.py
@@ -0,0 +1,10 @@
+"""Aula 4 - Primeiros comandos em Python3 (Python 3 | Mundo 1)
+
+002) Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas.
+"""
+
+nome = input('Qual o seu nome? ')
+
+print('Prazer em te conhecer,', nome + '. Seja bem vindo ao curso de Python.')
+print('É um prazer te conhecer {}!'.format(nome))
+print(f'Olá {nome}! Seja bem vindo ao curso de Python (Curso em Vídeo)')
